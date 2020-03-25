480aaf202d49c4391294fdada02d7a62b37bcc7c
RomainMichaux <59529466+RomainMichaux@users.noreply.github.com>
2020-03-25 11:50:38 +0100

    curses 6

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 import time
+from random import randint
 
 
 def affichage_titre(titre):
@@ -84,13 +85,14 @@ def jeu(win):
 
 	# Emission d'un beep  au début du jeu
 	curses.beep()
-
+	end = False
+  
 	# Tant que le joueur n'a pas quitter le jeu
-	while key != 27:
+	while key != 27 and not end:
 
 		key = controle(win, key)
 		snake, score = deplacement(win, score, key, snake, food)
-
+		end = perdu(win, snake)
 	return score
 
 		
@@ -147,7 +149,7 @@ def deplacement(win, score, key, snake, food):
 	# Suppression du dernier anneau du serpent.
 	# Sera conditionner plus tard au fait que le serpent mange ou pas une pomme
 	# Le score sera alors également mis à jour
-	last = snake.pop()
+	food, snake, last, score = mange_pomme(win, food, snake, score)
 
 
 	# Affichage de la tête à sa nouvelle position en bleu sur fond jaune
@@ -160,7 +162,7 @@ def deplacement(win, score, key, snake, food):
 	win.addstr(0, 2, 'Score : ' + str(snake[0][0]) + ' ')
 
 	# Attendre avant le pas suivant
-	vitesse = 1
+	vitesse = plus_vite(score)
 	win.timeout(150//vitesse)
 
 	# tuple contenant :
@@ -168,7 +170,102 @@ def deplacement(win, score, key, snake, food):
 	# - score en cours
 	return snake, score
 
+def mange_pomme(win, food, snake, score):
+	'''
+	Le serpent a-t-il mangé la pomme ?
+	paramètres :
+	  win : fenètre en cours
+	  food : liste des coordonnées de la pomme
+	  snake : liste des coordonnées des anneaux du serpent
+	  score : score en cours
+	retour :
+	  Tuple constitué de :
+	    - la liste des coordonnées actualisées de la pomme,
+	    - la liste des coordonnées du serpent,
+	    - la liste des coordonnées du dernier anneau à supprimer
+	    - le score en cours
+	'''
+	# initialisation de la liste contenant les coordonnées du dernier anneau du serpent
+	last = [0,0]
 
+	# Si le serpent a mangé la pomme
+	if snake[0] == food:
+		# Emettre un beep
+		curses.beep()
+
+		# incrémenter le score
+		score += 1
+
+		# Réactualiser les coordonnées de la pomme
+		# On recommence tant que les coordonnées de la pomme sont dans le serpent
+		while food in snake:
+
+			# On actualise au hasard les coordonnées de la pomme
+			# dans les limite de la fenêtre
+			# voir la documentation de la fonction window.getmaxyx()
+			food[0] = randint(1, win.getmaxyx()[0]-2)
+			food[1] = randint(1, win.getmaxyx()[1]-2)
+
+		# Affichage de la pomme aux nouvelles coordonnées en vert sur fond noir
+		win.addch(food[0], food[1], chr(211), curses.color_pair(2))
+		win.refresh()
+
+	# Sinon
+	else:
+		# Suppression du dernier anneau du serpent
+		last = snake.pop()
+
+	return food, snake, last, score
+
+def perdu(win, snake):
+	'''
+	Le serpent se mange-t-il la queue ?
+	paramètre :
+	  win : fenètre en cours
+	  snake : liste des positions en cours des anneaux du serpent
+	retourne :
+	  True si on perd, False sinon
+	'''
+
+	# initialisation de la variable end à retourner
+	end = False
+
+	# Si la tête du serpent est dans le corps
+	if snake[0] in snake[1:] :
+
+		# Afiicher "GAME OVER !" en blanc sur fond rouge au milieu de la fenêtre
+		curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_RED)
+		win.addstr(win.getmaxyx()[0]//2, win.getmaxyx()[1]//2-5, "GAME OVER !", curses.color_pair(4))
+		win.refresh()
+
+		# Emission d'une série de beep.
+		# Vous devrez écrire vous-même cette fonction !
+		beep_fin()
+
+		# On laisse 2 secondes au joueur pour s'assurer
+		# qu'il ait bien compris qu'il a perdu !
+		curses.napms(2000)
+		end = True
+	return end
+
+
+def plus_vite(score):
+	'''
+	Calcul de la vitesse du serpent
+	paramètre :
+	  score : score en cours
+	retourne :
+	  vitesse du serpent entre 1 et 10
+	'''
+
+	# vitesse est le quotient de la division entière de score par 5 ( + 1)
+	vitesse = score // 5 + 1
+
+	# Si vitesse est superieur à 10, alors vitesse = 10
+	if vitesse > 10:
+		vitesse = 10
+
+	return vitesse
 
 titre = ['  _______     _________ _    _  ____  _   _    _____ _   _          _  ________ ',
          ' |  __ \ \   / |__   __| |  | |/ __ \| \ | |  / ____| \ | |   /\   | |/ |  ____|',
